6a035938dbf1c08292340622f3914e3bdf030297
xiecang <xiecang@users.noreply.github.com>
2024-11-03 10:33:27 +0000

    deploy: xiecang/blog-astro@112b957ebb6540cf0fea80e59c642047b169bcee

---
+++ _astro/config.CsTvUyt6.js
@@ -0,0 +1,9 @@
+import{r as c}from"./index.DhYZZe0J.js";var i={exports:{}},n={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var m=c,_=Symbol.for("react.element"),f=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,y=m.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,d={key:!0,ref:!0,__self:!0,__source:!0};function l(t,e,s){var r,o={},a=null,p=null;s!==void 0&&(a=""+s),e.key!==void 0&&(a=""+e.key),e.ref!==void 0&&(p=e.ref);for(r in e)u.call(e,r)&&!d.hasOwnProperty(r)&&(o[r]=e[r]);if(t&&t.defaultProps)for(r in e=t.defaultProps,e)o[r]===void 0&&(o[r]=e[r]);return{$$typeof:_,type:t,key:a,ref:p,props:o,_owner:y.current}}n.Fragment=f;n.jsx=l;n.jsxs=l;i.exports=n;var E=i.exports;const O={lang:"en",langTag:["en-EN"]},g={repo:"xiecang/blog-comments",repoId:"MDEwOlJlcG9zaXRvcnkzODY5NjM1NzE=",category:"Announcements",categoryId:"DIC_kwDOFxCYc84Ccabp",mapping:"pathname",reactionsEnabled:"0",emitMetadata:"0",inputPosition:"bottom",lang:"en",loading:"lazy"};export{g as G,O as L,E as j};
